a00d7b6c0ed766a35d75be3bfb57db53abe225cb
Boper <85123713+boprr@users.noreply.github.com>
2024-11-05 20:50:01 +0100

    test

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
+import {CSS2DRenderer} from 'three/addons/renderers/CSS2DRenderer.js';
 import {FontLoader, OrbitControls} from 'three/examples/jsm/Addons.js';
 
 // SETUP
@@ -20,66 +21,63 @@ document.body.appendChild(renderer.domElement);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 
-// sphere (mars)
-const geo_sphere = new THREE.SphereGeometry(2, 100, 100);
-const mars_color =
-    new THREE.TextureLoader().load('planet/5672_mars_2k_color.jpg');
-const mars_normal =
-    new THREE.TextureLoader().load('planet/5672_mars_2k_normal.jpg');
-const mat_sphere =
-    new THREE.MeshStandardMaterial({map: mars_color, normalMap: mars_normal});
-const mesh_sphere = new THREE.Mesh(geo_sphere, mat_sphere);
+// sphere (earth)
+const earth_sphere = new THREE.SphereGeometry(2, 100, 100);
+const earth_color = new THREE.TextureLoader().load('planet/earth.jpg');
+const earth_bump = new THREE.TextureLoader().load('planet/earth_bump.jpg');
+const earth_spec = new THREE.TextureLoader().load('planet/earthspec.jpg');
+const earth_lights = new THREE.TextureLoader().load('planet/earthlights.jpg');
+
+const earth_clouds = new THREE.TextureLoader().load('planet/clouds.jpg');
+const clouds_sphere = new THREE.SphereGeometry(2.01, 100, 100);
+const clouds_mat = new THREE.MeshPhongMaterial({
+  alphaMap: earth_clouds,
+  opacity: 1,
+  transparent: true,
+});
+// clouds_mat.blending = THREE.AdditiveBlending;
+const clouds_mesh = new THREE.Mesh(clouds_sphere, clouds_mat);
+scene.add(clouds_mesh);
+
+const earth_mat = new THREE.MeshPhongMaterial({
+  map: earth_color,
+  // normalMap: earth_normal,
+  specularMap: earth_spec,
+  glowColor: 1,
+  bumpMap: earth_bump,
+  emissiveMap: earth_lights,
+  emissiveIntensity: 1,
+  emissive: true,
+  shininess: 100
+});
+
+const mesh_sphere = new THREE.Mesh(earth_sphere, earth_mat);
 scene.add(mesh_sphere);
 
+
 // light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
 directionalLight.position.set(5, 10, 7);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-const ambientLight = new THREE.AmbientLight(0x303030);  // Soft white light
+const ambientLight = new THREE.AmbientLight(0x000000);  // Soft white light
+ambientLight.intensity = 2;
 scene.add(ambientLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+// controls.enableRotate = false;  // temp
 controls.autoRotate = true;
 controls.autoRotateSpeed = 0.1;
 controls.enablePan = false;
-controls.enableZoom = false;
+// controls.enableZoom = false;
 
-const loader = new FontLoader();
-loader.load('nerd.json', function(font) {
-  const geometry = new TextGeometry('COMING SOON...', {
-    font: font,
-    size: 0.5,
-    height: 0.1,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 0.05,
-    bevelSize: 0.02,
-    bevelSegments: 5
-  });
-  const texture = new THREE.MeshStandardMaterial({color: 0xffffff});
-  const textMesh = new THREE.Mesh(geometry, texture);
-  textMesh.position.z = 2;
-  textMesh.position.x -= 2.5;
-  scene.add(textMesh);
-});
-
-
-
-// scroll
-function moveonscroll() {
-  const move = document.body.getBoundingClientRect().top;
-
-  mesh_sphere.rotation.x += move * -1;
-}
-
-document.body.onscroll = moveonscroll;
 
 
 function mainloop() {
   requestAnimationFrame(mainloop);
+
   controls.update();
   renderer.render(scene, camera);
 }
